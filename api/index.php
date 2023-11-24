36a87b294dd80f088d6e3c5e9e7666be42fb15a4
renan027027027 <143758728+renan027027027@users.noreply.github.com>
2023-11-24 12:27:02 -0300

    Update index.php

--- api/index.php
+++ api/index.php
@@ -2,5 +2,16 @@
 
 header('Content-Type: application/json');
 
-echo file_get_contents(__DIR__ . '/../src/index.ts');
+$numero = "6" + "7";
+
 ?>
+
+<html>
+  <head>
+    <title><?php echo $numero; ?></title>
+    <meta charset="utf-8">
+  </head>
+  <body>
+    Aqui vai o código HTML que fará seu site aparecer.
+  </body>
+</html>
